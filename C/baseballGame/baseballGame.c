266780bc7126641f2de15fc8e0caf71283c7d2c2
Segangs <woomahha@gmail.com>
2026-03-16 10:20:05 +0900

    03/16 폴더정리

--- C/baseballGame/baseballGame.c
+++ C/baseballGame/baseballGame.c
@@ -1,1 +1,63 @@
-extern int seed; // 전역변수가 있다고 명시
+#include "baseballGame.h"
+
+
+void generate_number(int* question)
+{
+    srand(time(NULL));
+    for (int i = 0; i < SIZE;++i){
+        question[i] = rand() % 9 + 1;
+        for (int j = 0; j < SIZE;++j){
+            if ( i == j ){
+                continue;
+            }
+            if (question[j] == question[i]){
+                --i;
+                break;
+            }
+        }
+    }
+    // 임시로 체크
+     for (int i = 0; i < SIZE;++i){
+         printf("%d ", question[i]);
+     }
+}
+
+void input_numbers(int* answer)
+{
+    printf("숫자를 %d개 넣으세요: ", SIZE);
+    for (int i = 0; i < SIZE;++i){
+        scanf("%d", &answer[i]);
+    }
+}
+
+bool check_result(const int *question, const int *answer, int *strike, int *ball)
+{
+
+    *strike = 0;
+    *ball = 0;
+
+    for (int i = 0; i < SIZE; i++)
+    {
+        for (int j = 0; j < SIZE; j++)
+        {
+            if (question[i] == answer[j])
+            {
+                if ( i == j )
+                {
+                    (*strike)++;
+                    printf("1 ");
+                }
+                else
+                {
+                    (*ball)++;
+                    printf(" ");
+                }
+            }
+        }
+    }
+    printf("Strike: %d \t Ball: %d\n", *strike, *ball);
+    if (*strike == SIZE)
+    {
+        return true;
+    }
+}
--- C/baseballGame/baseballGame.h
+++ C/baseballGame/baseballGame.h
@@ -1,5 +1,17 @@
+#pragma once
+
+// #ifndef BASEBALL_H
+// #define BASEBALL_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <stdbool.h>
 #define SIZE 3
+
+
+void generate_number(int* question);
+void input_numbers(int* answer);
+bool check_result(const int* question,const int* answer,int* strike,int* ball);
+
+// #endif
